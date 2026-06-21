3e0bb6a308ed01515e3ae2f187b6dd88909569e1
Allan Kenneth Rosal <mgrosal14@gmail.com>
2026-06-21 18:03:11 +0800

    Update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "15mb" }));
 app.use((req, res, next) => {
   res.setHeader("Cache-Control", "no-store");
   next();
